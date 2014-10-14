6f7d8c5c26cbc718f9c20d3cdb6eb158e26d8524
Mohit Singh <mohit.singh@osscube.com>
2014-10-14 18:07:27 +0530

    Some phpci standards

--- config/module.config.php
+++ config/module.config.php
@@ -2,30 +2,30 @@
 return array(
     'controllers' => array(
         'invokables' => array(
-            'ZF2Booster\Controller\ZF2Booster' => 'ZF2Booster\Controller\ZF2BoosterController',
-        ),
+            'ZF2Booster\Controller\ZF2Booster' => 'ZF2Booster\Controller\ZF2BoosterController'
+        )
     ),
     'router' => array(
         'routes' => array(
             'ZF2Booster' => array(
-                'type'    => 'segment',
+                'type' => 'segment',
                 'options' => array(
-                    'route'    => '/zf2booster[/:action][/:id]',
+                    'route' => '/zf2booster[/:action][/:id]',
                     'constraints' => array(
                         'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'id'     => '[0-9]+',
+                        'id' => '[0-9]+'
                     ),
                     'defaults' => array(
                         'controller' => 'ZF2Booster\Controller\ZF2Booster',
-                        'action'     => 'index',
-                    ),
-                ),
-            ),
-        ),
+                        'action' => 'index'
+                    )
+                )
+            )
+        )
     ),
     'view_manager' => array(
         'template_path_stack' => array(
-            'zf2Booster' => __DIR__ . '/../view',
-        ),
-    ),
+            'zf2Booster' => __DIR__ . '/../view'
+        )
+    )
 );
